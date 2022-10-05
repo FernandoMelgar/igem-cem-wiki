96ef25ba950fec754556fe7bdf029c41a69f5479
Humberto Carrillo <A01377246@tec.mx>
2022-10-05 16:49:12 -0500

    Added Cano Font@

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
 import * as React from "react";
-import {Routes, Route, Link} from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/home_page";
+import Laboratory from "./pages/laboratory_page";
 
 export default function App() {
-    return (
-        <div className="App">
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="about" element={<About/>}/>
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+      <Laboratory></Laboratory>
+    </div>
+  );
 }
 
-
 function About() {
-    return (
-        <>
-            <main>
-                <h2>Who are we?</h2>
-                <p>
-                    That feels like an existential question, don't you
-                    think?
-                </p>
-            </main>
-            <nav>
-                <Link to="/">Home</Link>
-            </nav>
-        </>
-    );
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>That feels like an existential question, don't you think?</p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
 }
---
+++ src/pages/laboratory_page.jsx
@@ -0,0 +1,9 @@
+function Laboratory() {
+  return (
+    <div>
+      <p className="lab">Welcome to the lab</p>
+    </div>
+  );
+}
+
+export default Laboratory;
