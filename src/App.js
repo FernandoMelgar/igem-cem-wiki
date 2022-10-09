2d68bb1341506d194323a490de5081bd459f9c47
Humberto Carrillo <A01377246@tec.mx>
2022-10-09 17:43:14 -0500

    Font retrieval has been relocated to server, unecessary files were removed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Routes, Route, Link} from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import LogBookPage from "./pages/laboratory/logbook_page";
 import SafetyProjectPage from "./pages/safety_project/safety_project_page";
 import Navbar from "./components/navbar";
@@ -11,42 +11,40 @@ import NotebookPage from "./pages/notebook/notebook_page";
 import HomePage from "./pages/home/home_page";
 import DemoPage from "./pages/demo_page";
 
+//  <Route path="/laboratory/safety" element={<SafetyProjectPage/>}/>
 export default function App() {
-    return (
-        <div className="App">
-            <Navbar/>
-            <Routes>
-                <Route path="/" element={<HomePage/>}/>
-                <Route path="about" element={<LogBookPage/>}/>
-                <Route path="/laboratory" element={<LaboratoryPage/>}/>
-                <Route path="/laboratory/safety" element={<SafetyProjectPage/>}/>
-                <Route path="/team" element={<TeamPage/>}/>
-                <Route path="/social-impact" element={<SocialImpactPage/>}/>
-                <Route path="/laboratory/notebook" element={<NotebookPage/>}/>
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes basename={process.env.PUBLIC_URL}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="about" element={<LogBookPage />} />
+        <Route path="/laboratory" element={<LaboratoryPage />} />
+        <Route path="/team" element={<TeamPage />} />
+        <Route path="/social-impact" element={<SocialImpactPage />} />
+        <Route path="/laboratory/notebook" element={<NotebookPage />} />
 
-                <Route path="/communication" element={<SocialImpactPage/>}/>
-                <Route path="/human-practices" element={<SocialImpactPage/>}/>
-                <Route path="/education" element={<SocialImpactPage/>}/>
-                <Route path="/sustainable" element={<SocialImpactPage/>}/>
-                <Route path="/demo" element={<DemoPage/>}/>
-
-
-            </Routes>
-            <IgemFooter/>
-        </div>
-    );
+        <Route path="/communication" element={<SocialImpactPage />} />
+        <Route path="/human-practices" element={<SocialImpactPage />} />
+        <Route path="/education" element={<SocialImpactPage />} />
+        <Route path="/sustainable" element={<SocialImpactPage />} />
+        <Route path="/demo" element={<DemoPage />} />
+      </Routes>
+      <IgemFooter />
+    </div>
+  );
 }
 
 function About() {
-    return (
-        <>
-            <main>
-                <h2>Who are we?</h2>
-                <p>That feels like an existential question, don't you think?</p>
-            </main>
-            <nav>
-                <Link to="/">Home</Link>
-            </nav>
-        </>
-    );
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>That feels like an existential question, don't you think?</p>
+      </main>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+    </>
+  );
 }
